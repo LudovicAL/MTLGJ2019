580e41f5f016a3ee5f90965cbd32959069c16b21
LudovicAL <ludovicaubut@hotmail.com>
2022-01-05 00:14:16 -0500

    Added a WebGL player

---
+++ Assets/Editor/PlayerHealthBarEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerHealthBar))]
+public class PlayerHealthBarEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		PlayerHealthBar playerHealthBar = (PlayerHealthBar)target;
+		if (GUILayout.Button("OnTakingDamage")) {
+			playerHealthBar.OnTakingDamage(playerHealthBar.player.playerId, playerHealthBar.player.playerId.currentHealth / playerHealthBar.player.playerId.maxHealth);
+		}
+	}
+}
---
+++ Assets/Editor/PlayerHealthEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerHealth))]
+public class PlayerHealthEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		PlayerHealth playerHealth = (PlayerHealth)target;
+		if (GUILayout.Button("TakeDamage")) {
+			playerHealth.TakeDamage(10f);
+		}
+	}
+}
